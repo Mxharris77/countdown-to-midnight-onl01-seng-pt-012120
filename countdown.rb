937fadfac5f854497cf3adcd938dd4e51ef45194
captivating-mapping-7374 <mxharris1094@yahoo.com>
2020-01-31 17:44:00 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -2,11 +2,11 @@
 
 
   def countdown(number)
-
+    number = 10
       while number > 0
         puts "#{number} SECOND(S)!"
           x -= 1
-      end
+        end
       puts "HAPPY NEW YEAR!"
   end
  
