9b24360ba3b4f93a4e32c6d34fb881cef8529535
captivating-mapping-7374 <mxharris1094@yahoo.com>
2020-01-31 17:44:54 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -2,13 +2,12 @@
 
 
   def countdown(number)
-    
-      while number > 0
-        puts "#{number} SECOND(S)!"
-          x -= 1
+     while number > 0
+         puts "#{number} SECOND(S)!"
+          number -= 1
         end
-      puts "HAPPY NEW YEAR!"
+    return "HAPPY NEW YEAR!"
   end
  
-def countdown_with_sleep(number)
+
 
